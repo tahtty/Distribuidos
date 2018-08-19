041a9e733d7131f67a86d54f87b4534720286a1f
Ahtty <aahtty@espol.edu.ec>
2018-08-19 17:20:07 -0500

    ruta y controlador para el metodo de las mejores imagenes /mejores .mejoresImages

--- backend/pruebasNode/controllers/ImageController.js
+++ backend/pruebasNode/controllers/ImageController.js
@@ -1,12 +1,38 @@
 const { grpcClient } = require('../config/service');
+const { Buffer } = require('buffer');
 
 module.exports = {
 
     mejores:function(req,res) {
       // var hello_proto = grpc.load(PROTO_PATH).helloworld();
-      grpcClient.hello({}, function(err, response) {
-      res.json({"Mensaje":response.body});
-    });
+      grpcClient.mejoresImagenes({}, function(err, response) {
+
+        if(err) res.json({'codigo':err.code,'details':err.details});
+        // response = [{descripcion:"",accesos:"",imagen:""}]
+        console.log(response);
+        let objeto_imagen = response.images.map(
+            (elemento)=>{
+              let devolver = {};
+              devolver.descripcion = elemento.descripcion;
+              devolver.cantidad_vistas = elemento.accesos;
+              devolver.imagen = Buffer.from(elemento.imagen).toString('base64')
+              // devolver.imagen = elemento.imagen;
+              return devolver;
+            }
+        );
+
+        res.json(objeto_imagen);
+        // res.json(response);
+
+      });
       // res.json({"img":"top10"});
+    },
+    hello:function(req,res) {
+      let nombre = req.query.name;
+      console.log(nombre);
+      grpcClient.hello({'name':nombre},(err,response)=>{
+        if(err) res.json({'codigo':err.code,'details':err.details});
+        res.json(response);
+      });
     }
 };
--- backend/pruebasNode/routes/images.js
+++ backend/pruebasNode/routes/images.js
@@ -3,6 +3,7 @@ var router = express.Router();
 var ImagenesController = require('../controllers/ImageController.js');
 
 router.get('/mejores', ImagenesController.mejores);
+router.get('/hello', ImagenesController.hello);
 
 
 module.exports = router;
