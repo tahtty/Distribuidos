02371b3b4eb2520d262f484d570e39a0e0fcb7df
Ahtty <aahtty@espol.edu.ec>
2018-08-19 17:25:10 -0500

    Agregada conexión con la dB para ActiveRecord

--- backend/pruebas/app/models.rb
+++ backend/pruebas/app/models.rb
@@ -1,5 +1,11 @@
 # app/models
 
+# frozen_string_literal: true
+require './config/db_config'
+ActiveRecord::Base.establish_connection(
+  DbConfig.config
+)
+
 Dir[File.expand_path './app/models/*.rb'].each do |file|
   require file
 end
