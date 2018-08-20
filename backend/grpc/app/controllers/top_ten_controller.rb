ba61ad37f6a36970e46cf7bed3871c9381960e59
Carlos León <carlos.leon.moran@gmail.com>
2018-08-20 02:25:27 +0000

    Envio de datos leidosde base

--- backend/grpc/app/controllers/top_ten_controller.rb
+++ backend/grpc/app/controllers/top_ten_controller.rb
@@ -4,25 +4,20 @@ class ImageController
     def self.get_top(request)
         images1 = ImagesModel.order("num_acceso desc").limit(10)
         puts images1.to_sql
-        
-        images1.each do |item|
+        images2 = []
+        images1.map { |item|
         # do whatever
-          puts item.url
-          open(item.url, "rb") do |read_file|
-            image = read_file.read
-              
-            str = Base64.encode64(image)
-            puts str
-              #img = Base64.decode64(str)
-              #redis.set( 'gatitos' , image )
-              #img = redis.get( 'gatitos')
-              #File.open("myimage2.gif", "wb") do |saved_file2|
-              #    saved_file2.write(img)
-              #end
-              #saved_file.write(read_file.read)
-          
-          end
-      end
+            obj = Hash.new
+            
+            open(item.url, "rb") do |read_file|
+                image = read_file.read
+                obj['imagen'] = image 
+            end
+            
+            obj['descripcion'] = item.descripcion
+            obj['accesos'] = item.num_acceso
+            images2 << obj
+        }
 
         images = []
         a = Hash.new
@@ -33,9 +28,6 @@ class ImageController
         b['accesos'] = 15
         b["descripcion"] = "Hola, soy Carlitos"
 
-        relPath = "gif_prueba.gif"
-        absPath = File.expand_path(relPath)
-  
         #File.open(absPath, "rb", File::RDONLY |
         #    File::NONBLOCK) do |read_file|
         #    image = read_file.read
@@ -51,8 +43,6 @@ class ImageController
         #    b["imagen"] = image
         #end
 
-        images << a
-        images << b
-        BuildGrpcObjects.convert_images_to_grpc_obj(images1)
+        BuildGrpcObjects.convert_images_to_grpc_obj(images2)
     end    
 end
